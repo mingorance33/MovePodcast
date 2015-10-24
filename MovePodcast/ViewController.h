f6e689ccaf4a291187b7cbc2c355c94e4b7a943b
Jose Antonio Vazquez Mingorance <joseantoniovazquez@iMac-de-Jose.local>
2015-10-24 09:31:20 +0200

    Fin proceso

--- MovePodcast/ViewController.h
+++ MovePodcast/ViewController.h
@@ -3,7 +3,7 @@
 //  MovePodcast
 //
 //  Created by Jose Antonio Vazquez Mingorance on 22/10/15.
-//  Copyright (c) 2015 Jose Vazquez. All rights reserved.
+//  Copyright (c) 2015 Jose Vazquez. All rights reserved
 //
 
 #import <Cocoa/Cocoa.h>
